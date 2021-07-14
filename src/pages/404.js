c710de1a7344cd43c219cd8341c18c19a4390a14
Coffee-Up <coffee.up.design@protonmail.com>
2021-07-14 23:09:18 +0200

    debug leaflet

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,43 +1,7 @@
-// import * as React from "react";
-// import { graphql } from "gatsby";
-// import {
-//   withPrismicUnpublishedPreview,
-//   componentResolverFromMap,
-// } from "gatsby-plugin-prismic-previews";
+import * as React from "react";
 
-// import { linkResolver } from "../linkResolver";
+const NotFoundPage = ({ data }) => {
+  return <div></div>;
+};
 
-// import PageTemplate from "./PageTemplate";
-
-// const NotFoundPage = ({ data }) => {
-//   const page = data.prismicPage;
-
-//   return (
-//     <div>
-//       <h1>{page.data.title.text}</h1>
-//     </div>
-//   );
-// };
-
-// export default withPrismicUnpublishedPreview(PageTemplate, [
-//   {
-//     repositoryName: "my-repository-name",
-//     linkResolver,
-//     componentResolver: componentResolverFromMap({
-//       page: PageTemplate,
-//     }),
-//   },
-// ]);
-
-// export const query = graphql`
-//   query NotFoundPage {
-//     prismicPage(id: { eq: "404" }) {
-//       _previewable
-//       data {
-//         title {
-//           text
-//         }
-//       }
-//     }
-//   }
-// `;
+export default NotFoundPage;
