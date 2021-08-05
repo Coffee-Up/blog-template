bba06882863699236062e851e43b3fe31e424f84
Coffee-Up <coffee.up.design@protonmail.com>
2021-08-05 16:00:08 +0200

    Prismic Leaflet added TemplatePage

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,7 +33,7 @@ exports.createPages = async ({ graphql, actions }) => {
   );
 
   result.data.allPrismicPageWeb.edges.forEach(({ node }) => {
-    if (!node.uid || node.uid === "homepage" || node.uid === "/404") return;
+    if (!node.uid || node.uid === "homepage" || node.uid === "404") return;
     createPage({
       path: `/${node.uid}`,
       component: path.resolve(`./src/pages/TemplatePageWeb.js`),
